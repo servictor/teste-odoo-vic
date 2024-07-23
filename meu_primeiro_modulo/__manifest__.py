41ba9d7395aa48b832f715eb8842e35c425a7778
servictor <143432375+servictor@users.noreply.github.com>
2024-07-23 13:18:21 -0300

    Add files via upload
    Subindo priemiros arquivos necessários para rodar os módulos customizados.

---
+++ meu_primeiro_modulo/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name":"meu_primeiro_modulo",
+    "author":"Victor",
+    "version":"1.0",
+    "description":"Primeiro módulo teste!",
+    "summary":"Objetivo de teste Julho, 23."
+}
---
+++ meu_primeiro_modulo/models/meu_primeiro_modelo.py
@@ -0,0 +1,6 @@
+from odoo import models
+
+
+class MeuPrimeiroModelo(models.Model):
+    _name = 'meu.primeiro.modelo'
+    _description = 'Este é o priemiro modelo criado no odoo.'
